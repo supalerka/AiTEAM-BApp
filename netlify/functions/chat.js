109fc77b9a43703addeb899c2df9769b37975d1a
supalerka <supalerk.a@gmail.com>
2026-05-20 12:23:34 +0700

    Update chat.js

--- netlify/functions/chat.js
+++ netlify/functions/chat.js
@@ -48,9 +48,8 @@ exports.handler = async (event) => {
   };
 
   try {
-    const model = "gemini-pro";
-    const url = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`;
-
+   const model = "gemini-1.5-flash";
+    const url = `https://generativelanguage.googleapis.com/v1/models/${model}:generateContent?key=${apiKey}`;
     const res = await fetch(url, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
